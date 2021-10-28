d12b543ce2a463b80286df1f166d1c262c1f92f3
Pranav <prn2108@gmail.com>
2021-10-28 23:20:17 +0530

    feat: homescreen product listing

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import HomeScreen from './Screens/HomeScreen';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Hello</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
