061b8ab45d1338c08282f557597539106bd74377
Pranav <prn2108@gmail.com>
2021-11-09 15:24:20 +0530

    feat: user sign up

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -31,7 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already exists!')
     }
 
-    const user = User.create({
+    const user = await User.create({
         name,
         email,
         password
