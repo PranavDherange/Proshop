dfbbdf4ca5d118388a68cb4afe6001761028fff6
Pranav <prn2108@gmail.com>
2021-11-12 20:41:24 +0530

    feat: admin update product

--- client/src/Screens/ProductEditScreen.js
+++ client/src/Screens/ProductEditScreen.js
@@ -5,8 +5,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import Message from '../Components/Message'
 import Loader from '../Components/Loader'
 import FormContainer from '../Components/FormContainer'
-import { listProductDetails } from '../actions/productActions'
+import { listProductDetails, updateProduct } from '../actions/productActions'
 import FileBase from "react-file-base64";
+import { PRODUCT_UPDATE_RESET } from '../constants/productConstants'
 
 const ProductEditScreen = ({ match, history }) => {
     const productId = match.params.id
@@ -24,23 +25,57 @@ const ProductEditScreen = ({ match, history }) => {
     const productDetails = useSelector((state) => state.productDetails)
     const { loading, error, product } = productDetails
 
+    const productUpdate = useSelector((state) => state.productUpdate)
+    const {
+        loading: loadingUpdate,
+        error: errorUpdate,
+        success: successUpdate,
+    } = productUpdate
+
     useEffect(() => {
-        if (!product.name || product._id !== productId) {
-            dispatch(listProductDetails(productId))
+        if (successUpdate) {
+            dispatch({ type: PRODUCT_UPDATE_RESET })
+            history.push('/admin/productlist')
         } else {
-            setName(product.name)
-            setPrice(product.price)
-            setImage(product.image)
-            setBrand(product.brand)
-            setCategory(product.category)
-            setCountInStock(product.countInSock)
-            setDescription(product.description)
+            if (!product.name || product._id !== productId) {
+                dispatch(listProductDetails(productId))
+            } else {
+                setName(product.name)
+                setPrice(product.price)
+                setImage(product.image)
+                setBrand(product.brand)
+                setCategory(product.category)
+                setCountInStock(product.countInSock)
+                setDescription(product.description)
+            }
         }
-    }, [dispatch, history, productId, product])
+    }, [dispatch, history, productId, product, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault()
         // UPDATE PRODUCT
+        console.log({
+            productId,
+            name,
+            price,
+            image,
+            brand,
+            category,
+            description,
+            countInStock,
+        })
+        dispatch(
+            updateProduct({
+                _id: productId,
+                name,
+                price,
+                image,
+                brand,
+                category,
+                description,
+                countInStock,
+            })
+        )
     }
 
     return (
@@ -50,6 +85,8 @@ const ProductEditScreen = ({ match, history }) => {
             </Link>
             <FormContainer>
                 <h1>Edit Product</h1>
+                {loadingUpdate && <Loader />}
+                {errorUpdate && <Message variant='danger'>{errorUpdate}</Message>}
                 {loading ? (
                     <Loader />
                 ) : error ? (
@@ -77,7 +114,7 @@ const ProductEditScreen = ({ match, history }) => {
                         </Form.Group>
 
                         <Form.Group controlId="image">
-                            <Form.Label>Image URL</Form.Label>
+                            <Form.Label>Image</Form.Label>
                             <Form.Control
                                 type="text"
                                 placeholder="Enter image url"
