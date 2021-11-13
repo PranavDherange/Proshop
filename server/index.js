83a05c7d0ccb8058f91648c0aa9da886d8feef34
Pranav <prn2108@gmail.com>
2021-11-13 16:44:19 +0530

    feat: create review endpoint

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import orderRoutes from './routes/orderRoutes.js'
+import morgan from 'morgan'
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -13,6 +14,10 @@ dotenv.config()
 connectDB()
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb' }));
